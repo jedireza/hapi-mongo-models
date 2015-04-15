c36cb5801c6c1969690c25bf46ed9cbb4daacc07
Michael Shick <michael.shick@gmail.com>
2015-04-15 11:40:12 -0400

    adding ability to register models dynamically during plugin init

--- index.js
+++ index.js
@@ -1,45 +1,64 @@
 var Path = require('path');
 var BaseModel = require('./lib/base-model');
 
+var internals = {};
+
+internals.requireModel = function (models) {
+    return function (modelName) {
+        if (typeof models[modelName] === 'string') {
+            if (models[modelName].charAt(0) === '/') {
+                models[modelName] = require(models[modelName]);
+            } else if (models[modelName].charAt(0) === '.') {
+                models[modelName] = require(Path.join(process.cwd(), models[modelName]));
+            } else {
+                models[modelName] = require(models[modelName]);
+            }
+        }
+    };
+};
+
+internals.after = function(options, models) {
+    var mongodb = options.mongodb;
+    var autoIndex = options.hasOwnProperty('autoIndex') ? options.autoIndex : true;
+
+    return function (server, next) {
+        BaseModel.connect(mongodb, function (err, db) {
+
+            if (err) {
+                server.log('Error connecting to MongoDB via BaseModel.');
+                return next(err);
+            }
+
+            Object.keys(models).forEach(function (key) {
+
+                if (autoIndex) {
+                    models[key].ensureIndexes();
+                }
+
+                server.expose(key, models[key]);
+            });
+
+            server.expose('BaseModel', BaseModel);
+
+            next();
+        });
+    };
+};
 
 exports.register = function (server, options, next) {
 
     var models = options.models || {};
-    var mongodb = options.mongodb;
-    var autoIndex = options.hasOwnProperty('autoIndex') ? options.autoIndex : true;
 
-    Object.keys(models).forEach(function (key) {
-        if (typeof models[key] === 'string') {
-            if (models[key].charAt(0) === '/') {
-                models[key] = require(models[key]);
-            } else if (models[key].charAt(0) === '.') {
-                models[key] = require(Path.join(process.cwd(), models[key]));
-            } else {
-                models[key] = require(models[key]);
-            }
-        }
+    Object.keys(models).forEach(internals.requireModel(models));
+
+    server.expose('addModel', function (modelName, model) {
+        models[modelName] = model;
+        internals.requireModel(models)(modelName);
     });
 
-    BaseModel.connect(mongodb, function (err, db) {
+    server.after(internals.after(options, models));
 
-        if (err) {
-            server.log('Error connecting to MongoDB via BaseModel.');
-            return next(err);
-        }
-
-        Object.keys(models).forEach(function (key) {
-
-            if (autoIndex) {
-                models[key].ensureIndexes();
-            }
-
-            server.expose(key, models[key]);
-        });
-
-        server.expose('BaseModel', BaseModel);
-
-        next();
-    });
+    next();
 };
 
 
